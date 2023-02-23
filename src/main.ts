351ca342c00ccc4c2d3176e8274389753516d6e5
Jack Woodrow <jackwoodrow99@gmail.com>
2023-02-23 16:31:06 -0500

    Update base64 variant from lieral to call to sodium

--- src/main.ts
+++ src/main.ts
@@ -22,24 +22,42 @@ export class Prism {
 
 	public generateIdentityKeys(): any {
 		let { publicKey, privateKey } = this.sodium.crypto_box_keypair();
-		this._IdentityKeys.public = this.sodium.to_base64(publicKey, 1);
-		this._IdentityKeys.private = this.sodium.to_base64(privateKey, 1);
+		this._IdentityKeys.public = this.sodium.to_base64(
+			publicKey,
+			this.sodium.base64_variants.ORIGINAL
+		);
+		this._IdentityKeys.private = this.sodium.to_base64(
+			privateKey,
+			this.sodium.base64_variants.ORIGINAL
+		);
 		return this.IdentityKeys;
 	}
 
 	public publicEncrypt(message: any, recipientPublicKey: any): any {
 		let cypherText = this.sodium.crypto_box_seal(
 			JSON.stringify(message),
-			this.sodium.from_base64(recipientPublicKey, 1)
+			this.sodium.from_base64(
+				recipientPublicKey,
+				this.sodium.base64_variants.ORIGINAL
+			)
 		);
-		return this.sodium.to_base64(cypherText, 1);
+		return this.sodium.to_base64(
+			cypherText,
+			this.sodium.base64_variants.ORIGINAL
+		);
 	}
 
 	public publicDecrypt(cypherText: any): any {
 		let plainText = this.sodium.crypto_box_seal_open(
-			this.sodium.from_base64(cypherText, 1),
-			this.sodium.from_base64(this.IdentityKeys.public, 1),
-			this.sodium.from_base64(this.IdentityKeys.private, 1)
+			this.sodium.from_base64(cypherText, this.sodium.base64_variants.ORIGINAL),
+			this.sodium.from_base64(
+				this.IdentityKeys.public,
+				this.sodium.base64_variants.ORIGINAL
+			),
+			this.sodium.from_base64(
+				this.IdentityKeys.private,
+				this.sodium.base64_variants.ORIGINAL
+			)
 		);
 		return JSON.parse(this.sodium.to_string(plainText));
 	}
@@ -50,13 +68,22 @@ export class Prism {
 		let cypherText = this.sodium.crypto_box_easy(
 			JSON.stringify(data),
 			nonce,
-			this.sodium.from_base64(recipientPublicKey, 1),
-			this.sodium.from_base64(this.IdentityKeys.private, 1)
+			this.sodium.from_base64(
+				recipientPublicKey,
+				this.sodium.base64_variants.ORIGINAL
+			),
+			this.sodium.from_base64(
+				this.IdentityKeys.private,
+				this.sodium.base64_variants.ORIGINAL
+			)
 		);
 
 		return {
-			nonce: this.sodium.to_base64(nonce, 1),
-			cypherText: this.sodium.to_base64(cypherText, 1),
+			nonce: this.sodium.to_base64(nonce, this.sodium.base64_variants.ORIGINAL),
+			cypherText: this.sodium.to_base64(
+				cypherText,
+				this.sodium.base64_variants.ORIGINAL
+			),
 		};
 	}
 
@@ -64,10 +91,16 @@ export class Prism {
 		const data = JSON.parse(
 			this.sodium.to_string(
 				this.sodium.crypto_box_open_easy(
-					this.sodium.from_base64(cypherText, 1),
-					this.sodium.from_base64(nonce, 1),
-					this.sodium.from_base64(from, 1),
-					this.sodium.from_base64(this.IdentityKeys.private, 1)
+					this.sodium.from_base64(
+						cypherText,
+						this.sodium.base64_variants.ORIGINAL
+					),
+					this.sodium.from_base64(nonce, this.sodium.base64_variants.ORIGINAL),
+					this.sodium.from_base64(from, this.sodium.base64_variants.ORIGINAL),
+					this.sodium.from_base64(
+						this.IdentityKeys.private,
+						this.sodium.base64_variants.ORIGINAL
+					)
 				)
 			)
 		);
@@ -78,7 +111,7 @@ export class Prism {
 		if (key == null) {
 			key = this.sodium.crypto_aead_chacha20poly1305_keygen();
 		} else {
-			key = this.sodium.from_base64(key, 1);
+			key = this.sodium.from_base64(key, this.sodium.base64_variants.ORIGINAL);
 		}
 
 		let nonce: any = this.sodium.randombytes_buf(
@@ -95,19 +128,22 @@ export class Prism {
 			);
 
 		return {
-			key: this.sodium.to_base64(key, 1),
-			nonce: this.sodium.to_base64(nonce, 1),
-			cypherText: this.sodium.to_base64(cypherText, 1),
+			key: this.sodium.to_base64(key, this.sodium.base64_variants.ORIGINAL),
+			nonce: this.sodium.to_base64(nonce, this.sodium.base64_variants.ORIGINAL),
+			cypherText: this.sodium.to_base64(
+				cypherText,
+				this.sodium.base64_variants.ORIGINAL
+			),
 		};
 	}
 
 	public symmetricDecrypt(key: any, nonce: any, cypherText: any): any {
 		let message: any = this.sodium.crypto_aead_xchacha20poly1305_ietf_decrypt(
 			null,
-			this.sodium.from_base64(cypherText, 1),
+			this.sodium.from_base64(cypherText, this.sodium.base64_variants.ORIGINAL),
 			null,
-			this.sodium.from_base64(nonce, 1),
-			this.sodium.from_base64(key, 1)
+			this.sodium.from_base64(nonce, this.sodium.base64_variants.ORIGINAL),
+			this.sodium.from_base64(key, this.sodium.base64_variants.ORIGINAL)
 		);
 		return JSON.parse(this.sodium.to_string(message));
 	}
@@ -115,8 +151,14 @@ export class Prism {
 	public generateSessionKeys(): any {
 		let { publicKey, privateKey }: any = this.sodium.crypto_kx_keypair();
 		return {
-			publicKey: this.sodium.to_base64(publicKey, 1),
-			privateKey: this.sodium.to_base64(privateKey, 1),
+			publicKey: this.sodium.to_base64(
+				publicKey,
+				this.sodium.base64_variants.ORIGINAL
+			),
+			privateKey: this.sodium.to_base64(
+				privateKey,
+				this.sodium.base64_variants.ORIGINAL
+			),
 		};
 	}
 
@@ -126,14 +168,29 @@ export class Prism {
 		partnerPublicKey: any
 	): any {
 		let { sharedRx, sharedTx }: any = this.sodium.crypto_kx_client_session_keys(
-			this.sodium.from_base64(sessionPublicKey, 1),
-			this.sodium.from_base64(sessionPrivateKey, 1),
-			this.sodium.from_base64(partnerPublicKey, 1)
+			this.sodium.from_base64(
+				sessionPublicKey,
+				this.sodium.base64_variants.ORIGINAL
+			),
+			this.sodium.from_base64(
+				sessionPrivateKey,
+				this.sodium.base64_variants.ORIGINAL
+			),
+			this.sodium.from_base64(
+				partnerPublicKey,
+				this.sodium.base64_variants.ORIGINAL
+			)
 		);
 
 		return {
-			receiveKey: this.sodium.to_base64(sharedRx, 1),
-			sendKey: this.sodium.to_base64(sharedTx, 1),
+			receiveKey: this.sodium.to_base64(
+				sharedRx,
+				this.sodium.base64_variants.ORIGINAL
+			),
+			sendKey: this.sodium.to_base64(
+				sharedTx,
+				this.sodium.base64_variants.ORIGINAL
+			),
 		};
 	}
 
@@ -143,14 +200,29 @@ export class Prism {
 		partnerPublicKey: any
 	): any {
 		let { sharedRx, sharedTx }: any = this.sodium.crypto_kx_server_session_keys(
-			this.sodium.from_base64(sessionPublicKey, 1),
-			this.sodium.from_base64(sessionPrivateKey, 1),
-			this.sodium.from_base64(partnerPublicKey, 1)
+			this.sodium.from_base64(
+				sessionPublicKey,
+				this.sodium.base64_variants.ORIGINAL
+			),
+			this.sodium.from_base64(
+				sessionPrivateKey,
+				this.sodium.base64_variants.ORIGINAL
+			),
+			this.sodium.from_base64(
+				partnerPublicKey,
+				this.sodium.base64_variants.ORIGINAL
+			)
 		);
 
 		return {
-			receiveKey: this.sodium.to_base64(sharedRx, 1),
-			sendKey: this.sodium.to_base64(sharedTx, 1),
+			receiveKey: this.sodium.to_base64(
+				sharedRx,
+				this.sodium.base64_variants.ORIGINAL
+			),
+			sendKey: this.sodium.to_base64(
+				sharedTx,
+				this.sodium.base64_variants.ORIGINAL
+			),
 		};
 	}
 
@@ -163,10 +235,13 @@ export class Prism {
 			this.sodium.crypto_kdf_KEYBYTES,
 			count,
 			context,
-			this.sodium.from_base64(key, 1)
+			this.sodium.from_base64(key, this.sodium.base64_variants.ORIGINAL)
 		);
 
-		return this.sodium.to_base64(derivedKey, 1);
+		return this.sodium.to_base64(
+			derivedKey,
+			this.sodium.base64_variants.ORIGINAL
+		);
 	}
 
 	public prismEncrypt_Layer1(data: any, sharedSessionKeySend: any): any {
@@ -201,13 +276,25 @@ export class Prism {
 				data: layer_1_cypherText,
 			}),
 			layer_2_nonce,
-			this.sodium.from_base64(recipientPublicIdentityKey, 1),
-			this.sodium.from_base64(this.IdentityKeys.private, 1)
+			this.sodium.from_base64(
+				recipientPublicIdentityKey,
+				this.sodium.base64_variants.ORIGINAL
+			),
+			this.sodium.from_base64(
+				this.IdentityKeys.private,
+				this.sodium.base64_variants.ORIGINAL
+			)
 		);
 
 		return {
-			nonce: this.sodium.to_base64(layer_2_nonce, 1),
-			cypherText: this.sodium.to_base64(layer_2_cypherText, 1),
+			nonce: this.sodium.to_base64(
+				layer_2_nonce,
+				this.sodium.base64_variants.ORIGINAL
+			),
+			cypherText: this.sodium.to_base64(
+				layer_2_cypherText,
+				this.sodium.base64_variants.ORIGINAL
+			),
 		};
 	}
 	public prismEncrypt_Layer3(layer_2_nonce: any, layer_2_cypherText: any): any {
@@ -250,10 +337,16 @@ export class Prism {
 		const symmetricDecrypted = JSON.parse(
 			this.sodium.to_string(
 				this.sodium.crypto_box_open_easy(
-					this.sodium.from_base64(cypherText, 1),
-					this.sodium.from_base64(nonce, 1),
-					this.sodium.from_base64(from, 1),
-					this.sodium.from_base64(this.IdentityKeys.private, 1)
+					this.sodium.from_base64(
+						cypherText,
+						this.sodium.base64_variants.ORIGINAL
+					),
+					this.sodium.from_base64(nonce, this.sodium.base64_variants.ORIGINAL),
+					this.sodium.from_base64(from, this.sodium.base64_variants.ORIGINAL),
+					this.sodium.from_base64(
+						this.IdentityKeys.private,
+						this.sodium.base64_variants.ORIGINAL
+					)
 				)
 			)
 		);
